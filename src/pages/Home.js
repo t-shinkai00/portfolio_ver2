7ee569f73fde50a8749882a6ca2608008e86e7ec
t-shinkai00 <t.shinkai00@gmail.com>
2020-12-05 23:59:19 +0900

    add useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  IonButton,
   IonContent,
   IonHeader,
   IonPage,
@@ -10,16 +11,59 @@ import {
 import "../theme/variables.css";
 
 const Home = () => {
-  const [state, setstate] = useState(initialState);
+  const [color, setColor] = useState("lightgreen");
+  const [count, setcount] = useState(0);
+  const changeColor = () => {
+    setcount(count + 1);
+    if (count % 3 == 0) {
+      setColor("lightgreen");
+    } else if (count % 3 == 1) {
+      setColor("lightyellow");
+    } else {
+      setColor("green");
+    }
+  };
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">ホーム</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <div style={{ backgroundColor: "rgb(0,0,255)" }}>
+        <div style={{ backgroundColor: color }}>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">ホーム</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonButton
+            onClick={() => {
+              changeColor();
+            }}
+          >
+            push!!
+          </IonButton>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
           <h1>hello</h1>
         </div>
       </IonContent>
